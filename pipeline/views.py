457bfb6e0098a9a620e88ef5582dbb71dda607b8
matheusstyt <matheus20galdino@gmail.com>
2022-11-22 17:52:54 -0400

    Novamente o CRUD do pipeline..

--- pipeline/views.py
+++ pipeline/views.py
@@ -3,5 +3,25 @@ from .models import PipelineVendas
 # Create your views here.
 def PipelineView(request):
     pipeline = PipelineVendas.objects.all()
-
-    return render(request, 'pipeline/index.html', {'dados' : pipeline})
+    lista = [
+        "Cliente",
+        "Fase",
+        "Descrição/Proposta",
+        "OMIE",
+        "NF_ Emitidas",
+        "Coletor QTDA",
+        "Data_ envio Proposta",
+        "Revisão + Recente",
+        "Data do P.C",
+        "Recorrencia",
+        "Perpetua",
+        "Hardware",
+        "Serviços",
+        "TOTAL PREVISTO",
+        "FATURADO NOVEMBRO",
+        "ENTREGA",
+        "Pagamento",
+        "Contato",
+        "OBS",
+    ]
+    return render(request, 'pipeline/index.html', {'dados' : pipeline, 'lista': lista})
